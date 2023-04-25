2a65a759326e70a9e230d9124a4d05588acb7261
Canse <49596442+CanseFr@users.noreply.github.com>
2023-04-25 10:55:04 +0200

    Update models.py

--- pizzamama/menu/models.py
+++ pizzamama/menu/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Models menu.
 class Pizza(models.Model):
-    nom = models.CharField(max_length=200) # nom est egale a une chaine de caractere dont le nombre de char doit etre specifié
+    nom = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=400)
     prix = models.FloatField(default=0)
     vegetarienne = models.BooleanField(default=False)
